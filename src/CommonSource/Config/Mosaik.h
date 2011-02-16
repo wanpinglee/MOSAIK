d7f031a97a3e693e5b1bfc5fc4a7cf5661d2a6b7
wanpinglee <wanping.lee@gmail.com>
2011-02-16 17:24:07 -0500

    Version 2.0.32

--- src/CommonSource/Config/Mosaik.h
+++ src/CommonSource/Config/Mosaik.h
@@ -20,12 +20,12 @@
 // MOSAIK version
 // ==============
 
-#define MOSAIK_VERSION_DATE "2011-02-11"
+#define MOSAIK_VERSION_DATE "2011-02-16"
 
 // adopt a major.minor.build version number [1].[1].[3]
 const unsigned char  MOSAIK_MAJOR_VERSION = 2;
 const unsigned char  MOSAIK_MINOR_VERSION = 0;
-const unsigned short MOSAIK_BUILD_VERSION = 24;
+const unsigned short MOSAIK_BUILD_VERSION = 32;
 
 // ================================
 // Platform specific variable sizes
