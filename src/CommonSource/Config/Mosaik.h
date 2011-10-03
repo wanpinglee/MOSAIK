4df160e70f0cc237cea0e93e3ca82dcfaec544a7
wanpinglee <wanping.lee@gmail.com>
2011-10-03 10:23:57 -0400

    Version 2.1.26

--- src/CommonSource/Config/Mosaik.h
+++ src/CommonSource/Config/Mosaik.h
@@ -20,12 +20,12 @@
 // MOSAIK version
 // ==============
 
-#define MOSAIK_VERSION_DATE "2011-09-29"
+#define MOSAIK_VERSION_DATE "2011-10-03"
 
 // adopt a major.minor.build version number [1].[1].[3]
 const unsigned char  MOSAIK_MAJOR_VERSION = 2;
 const unsigned char  MOSAIK_MINOR_VERSION = 1;
-const unsigned short MOSAIK_BUILD_VERSION = 7;
+const unsigned short MOSAIK_BUILD_VERSION = 26;
 
 // ================================
 // Platform specific variable sizes
--- src/MosaikJump/JumpMain.cpp
+++ src/MosaikJump/JumpMain.cpp
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]) {
 
 	printf("------------------------------------------------------------------------------\n");
 	printf("Mosaik"); CConsole::Red(); printf("Jump"); CConsole::Reset();
-	printf(" %u.%u.%u                                                    %s\n", 
+	printf(" %u.%u.%u                                                   %s\n", 
 		MOSAIK_MAJOR_VERSION, MOSAIK_MINOR_VERSION, MOSAIK_BUILD_VERSION, MOSAIK_VERSION_DATE);
 	printf("Michael Stromberg & Wan-Ping Lee  Marth Lab, Boston College Biology Department\n");
 	printf("------------------------------------------------------------------------------\n\n");
