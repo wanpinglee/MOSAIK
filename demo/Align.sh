061dedd8af16be55b8a93c9f6a7957fedf869e72
wanpinglee <wanping.lee@gmail.com>
2012-09-18 14:22:45 -0400

    modified:   Align.sh 	modified:   Build.sh 	modified:   RetrainMQ.sh

--- demo/Align.sh
+++ demo/Align.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-### create ../bin/MosaikAligner
+### Create ../bin/MosaikAligner
 # cd ../src
 # make
 ###
 
-# align the reads
+# Align the reads
 ANN_PATH=../src/networkFile
 ../bin/MosaikAligner -in fastq/read.mkb -out fastq/read.mka -ia reference/e.coli.dat -annpe $ANN_PATH/2.1.26.pe.100.0065.ann -annse $ANN_PATH/2.1.26.se.100.005.ann
-# you may need to use the jump database for large genome (> 100 million basepair)
+# You may need to use the jump database for large genome (> 100 million basepair)
 #../bin/MosaikAligner -in fastq/read.mkb -out fastq/read.mka -ia reference/e.coli.dat -annpe $ANN_PATH/2.1.26.pe.100.0065.ann -annse $ANN_PATH/2.1.26.se.100.005.ann -j reference/e.coli.15
 
--- demo/Build.sh
+++ demo/Build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-### create ../bin/MosaikBuild
+### Create ../bin/MosaikBuild
 # cd ../src
 # make
 ###
 
-# convert the reference sequence to our binary format
+# Convert the reference sequence to our binary format
 ../bin/MosaikBuild -fr reference/e.coli.fa -oa reference/e.coli.dat
-# you may need to create the jump database for large genome (> 100 million basepair)
+# You may need to create the jump database for large genome (> 100 million basepair)
 #../bin/MosaikJump -ia reference/e.coli.dat -hs 15 -out reference/e.coli.15
 
-# convert the reads to our binary format
+# Convert the reads to our binary format
 ../bin/MosaikBuild -q fastq/mate1.fq -q2 fastq/mate2.fq -out fastq/read.mkb -st illumina
