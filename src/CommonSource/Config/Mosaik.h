32ce1df5d67774ca637b87a7ec3a666e71a07f2c
wanpinglee <wanping.lee@gmail.com>
2013-02-13 15:24:11 -0500

    Version 2.1.78 new neural networks, 2.1.78.ann

--- src/CommonSource/Config/Mosaik.h
+++ src/CommonSource/Config/Mosaik.h
@@ -20,12 +20,12 @@
 // MOSAIK version
 // ==============
 
-#define MOSAIK_VERSION_DATE "2013-01-18"
+#define MOSAIK_VERSION_DATE "2013-02-13"
 
 // adopt a major.minor.build version number [1].[1].[3]
 const unsigned char  MOSAIK_MAJOR_VERSION = 2;
 const unsigned char  MOSAIK_MINOR_VERSION = 1;
-const unsigned short MOSAIK_BUILD_VERSION = 75;
+const unsigned short MOSAIK_BUILD_VERSION = 78;
 
 // ================================
 // Platform specific variable sizes
