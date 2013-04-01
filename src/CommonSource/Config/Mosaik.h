2287bfb49dba1c2cd82088cd88b103cd1ebe83a7
wanpinglee <wanping.lee@gmail.com>
2013-04-01 11:12:33 -0400

    Update README Version 2.1.80

--- src/CommonSource/Config/Mosaik.h
+++ src/CommonSource/Config/Mosaik.h
@@ -25,7 +25,7 @@
 // adopt a major.minor.build version number [1].[1].[3]
 const unsigned char  MOSAIK_MAJOR_VERSION = 2;
 const unsigned char  MOSAIK_MINOR_VERSION = 1;
-const unsigned short MOSAIK_BUILD_VERSION = 79;
+const unsigned short MOSAIK_BUILD_VERSION = 80;
 
 // ================================
 // Platform specific variable sizes
