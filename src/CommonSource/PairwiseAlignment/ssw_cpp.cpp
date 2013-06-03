a07599d4e7ebb584c9ac9fd2c6ce9ce8c8b0fd70
wanpinglee <wanping.lee@bc.edu>
2013-06-03 17:43:38 -0400

    Make aligning N's be mismatches

--- src/CommonSource/PairwiseAlignment/ssw_cpp.cpp
+++ src/CommonSource/PairwiseAlignment/ssw_cpp.cpp
@@ -41,12 +41,12 @@ void BuildSwScoreMatrix(const uint8_t& match_score,
       matrix[id] = ((i == j) ? match_score : static_cast<int8_t>(-mismatch_penalty));
       ++id;
     }
-    matrix[id] = 0;
+    matrix[id] = static_cast<int8_t>(-mismatch_penalty);
     ++id;
   }
 
   for (int i = 0; i < 5; ++i)
-    matrix[id++] = 0;
+    matrix[id++] = static_cast<int8_t>(-mismatch_penalty);
     
 }
 
